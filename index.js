d5d4c234509d5fd4a8742af56111f3dc43bd07b2
nitro-neal <groessler24@gmail.com>
2019-12-13 12:06:27 -0600

    Adding cors config

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ const app = express();
 
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.send("Send Email Homepage");
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
