8a8fdad0676da7d3879b0f79a36533019fa86c63
Neal Roessler <groessler24@gmail.com>
2019-12-15 14:39:46 -0600

    Adding spashpage, adding logo

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 
 // app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.static("public"));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
